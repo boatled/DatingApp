d95c1f95f3357bd75214fa3b3759167274f1dd26
unknown <itsdexu5@gmail.com>
2019-11-22 22:36:18 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<Value> Values { get; set; }
+        // Values will be the table name in SQL
+        // Conventionto make it plural
+        
+    }
+}
